8dfcdb4f8488bc068e364cd1630038778aa65728
pinardZ <z1273087123@gmail.com>
2026-02-24 07:24:14 +0800

    feat: update BEACON_VALIDATORS_URL

--- web-server/server.js
+++ web-server/server.js
@@ -149,5 +149,5 @@ app.get('/api/events', (req, res) => {
 
 // 启动服务器
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
